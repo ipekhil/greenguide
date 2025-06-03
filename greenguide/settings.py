3069d20076ccd3b9e7423b44bc90a94f0a200393
example <email@example.com>
2025-06-03 14:43:40 +0300

    static img

--- greenguide/settings.py
+++ greenguide/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-7e^@r(6q7fws_tq3xl48ipj@w7*8(!zo3dq-^an02ko)_gtv!v"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+CSRF_TRUSTED_ORIGINS = ['https://greenguide.pythonanywhere.com']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['greenguide.pythonanywhere.com']
 
 
 # Application definition
@@ -118,11 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.greenguide.com.tr'
